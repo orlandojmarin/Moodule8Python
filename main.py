65993c1eca91f988efb7e23c89ebb8f0ecd6de7e
orlandomarin <orlandomarin@orlandos-air.lan>
2024-06-08 16:38:37 -0400

    Eighth Python Program - Modules

---
+++ main.py
@@ -0,0 +1,14 @@
+# Write a function called calculateQuotient() that has two paramatera a and b.
+# Within the function, write a return statement to return the value of a / b.
+# After coding the function, write a statement to call the function using 12
+# and 3 as the argumeents. Assign the result to a variable called result and
+# print the value of the result
+
+# create the calculate quotient function
+def calculateQuotient(a, b):
+    return a / b
+    
+
+# call the calculate Quotient function with 12 and 3 as arguments
+result = calculateQuotient(12, 3)
+print(f"The result is {result}, Orlando!")
